bdd7468a2c4cd47c74e3d777117c8938a6981cda
udit singh <jarves.pro@gmail.com>
2023-05-21 16:31:23 +0530

    Update voiceTime.js

--- events/voiceTime.js
+++ events/voiceTime.js
@@ -16,22 +16,18 @@ function convertMsToTime(milliseconds) {
   let seconds = Math.floor(milliseconds / 1000);
   let minutes = Math.floor(seconds / 60);
   let hours = Math.floor(minutes / 60);
-
   seconds = seconds % 60;
   minutes = minutes % 60;
-
-  // 👇️ If you don't want to roll hours over, e.g. 24 to 00
-  // 👇️ comment (or remove) the line below
-  // commenting next line gets you `24:00:00` instead of `00:00:00`
-  // or `36:15:31` instead of `12:15:31`, etc.
   hours = hours % 24;
+
 
   return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
     seconds
   )}`;
 }
+const currentTime = new Date().toLocaleTimeString();
+console.log("Current Time:", currentTime);
 let currentDate = new Date().toJSON().slice(0, 10);
-
 let bot_start_time = Date.now();
 
 const logged_data = new Map();
@@ -157,7 +153,7 @@ module.exports = {
             );
 
           logger.info(
-            `${newState.member.user.tag} left ${oldState.channel.name}`
+            `${newState.member.user.tag} left ${oldState.channel.name} at ${currentTime}`
           );
 
           logger.info(
